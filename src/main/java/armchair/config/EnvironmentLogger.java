4ebebdbd0312dbf07bf54235e7c782690eae46e6
hstewart38 <hstewart38@gatech.edu>
2026-03-19 22:53:14 -0400

    133: Fix EnvironmentLogger crash when env vars are missing

--- src/main/java/armchair/config/EnvironmentLogger.java
+++ src/main/java/armchair/config/EnvironmentLogger.java
@@ -2,8 +2,8 @@ package armchair.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,29 +11,22 @@ public class EnvironmentLogger implements CommandLineRunner {
 
     private static final Logger log = LoggerFactory.getLogger(EnvironmentLogger.class);
 
-    @Value("${spring.security.oauth2.client.registration.google.client-id:}")
-    private String googleClientId;
+    private final Environment env;
 
-    @Value("${spring.security.oauth2.client.registration.google.client-secret:}")
-    private String googleClientSecret;
-
-    @Value("${spring.security.oauth2.client.registration.github.client-id:}")
-    private String githubClientId;
-
-    @Value("${spring.security.oauth2.client.registration.github.client-secret:}")
-    private String githubClientSecret;
-
-    @Value("${spring.security.oauth2.client.registration.microsoft.client-id:}")
-    private String microsoftClientId;
-
-    @Value("${spring.security.oauth2.client.registration.microsoft.client-secret:}")
-    private String microsoftClientSecret;
-
-    @Value("${spring.datasource.url:}")
-    private String datasourceUrl;
+    public EnvironmentLogger(Environment env) {
+        this.env = env;
+    }
 
     @Override
     public void run(String... args) {
+        String googleClientId = resolve("spring.security.oauth2.client.registration.google.client-id");
+        String googleClientSecret = resolve("spring.security.oauth2.client.registration.google.client-secret");
+        String githubClientId = resolve("spring.security.oauth2.client.registration.github.client-id");
+        String githubClientSecret = resolve("spring.security.oauth2.client.registration.github.client-secret");
+        String microsoftClientId = resolve("spring.security.oauth2.client.registration.microsoft.client-id");
+        String microsoftClientSecret = resolve("spring.security.oauth2.client.registration.microsoft.client-secret");
+        String datasourceUrl = resolve("spring.datasource.url");
+
         log.info("Configured credentials: GOOGLE_CLIENT_ID={}, GOOGLE_CLIENT_SECRET={}, "
                 + "GITHUB_CLIENT_ID={}, GITHUB_CLIENT_SECRET={}, "
                 + "MICROSOFT_CLIENT_ID={}, MICROSOFT_CLIENT_SECRET={}, "
@@ -45,6 +38,15 @@ public class EnvironmentLogger implements CommandLineRunner {
                 preview(microsoftClientId),
                 preview(microsoftClientSecret),
                 preview(datasourceUrl));
+    }
+
+    private String resolve(String key) {
+        try {
+            return env.getProperty(key);
+        } catch (Exception e) {
+            log.debug("Could not resolve property {}: {}", key, e.getMessage());
+            return null;
+        }
     }
 
     private String preview(String value) {
